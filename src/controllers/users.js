749e84fd1b3df7c82cd5c2878b2679e2f1b35003
Ling Sang <lingsang.tse@mail.utoronto.ca>
2020-04-02 12:46:32 -0400

    step 2 take 1

---
+++ src/controllers/users.js
@@ -0,0 +1,11 @@
+exports.findAll = function (req, res){
+    res.send ([
+
+    ]);
+}
+
+exports.findById = function () {};
+exports.add = function () {};
+exports.update = function (){};
+exports.delete = function (){};
+
